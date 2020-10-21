f6d5c001c9bc7eccffcdadb17bd8f54999d1c0d9
kinggeert <geertbruls@outlook.com>
2020-10-21 19:17:02 +0200

    started project
    I started the project

---
+++ src/main/java/me/kinggeert/magicwands/magic_wands/Magic_wands.java
@@ -0,0 +1,17 @@
+package me.kinggeert.magicwands.magic_wands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Magic_wands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
