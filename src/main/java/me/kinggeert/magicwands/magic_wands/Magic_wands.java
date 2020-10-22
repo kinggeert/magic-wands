5df4f9cef49cb752d2c6ad6abbd83536f7795c1c
kinggeert <geertbruls@outlook.com>
2020-10-22 13:09:04 +0200

    test
    test

--- src/main/java/me/kinggeert/magicwands/magic_wands/Magic_wands.java
+++ src/main/java/me/kinggeert/magicwands/magic_wands/Magic_wands.java
@@ -1,17 +1,34 @@
 package me.kinggeert.magicwands.magic_wands;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Bukkit;
+
 
 public final class Magic_wands extends JavaPlugin {
 
+
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        runnable();
+
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void runnable() {
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                Bukkit.broadcastMessage("test");
+
+            }
+        }.runTaskTimerAsynchronously(this, 0, 20);
     }
 }
